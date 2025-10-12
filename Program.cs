a0dbd06900f6ea74763a06ad1aea7540af180032
nemonology <nemonology@mail.ru>
2025-10-12 20:01:52 +0900

    Добавлен тестовый код, создающий и сохраняющий изображение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+Image<Rgb24> image = new(32, 32);
+
+using var imageFileStream = File.OpenWrite("test.bmp");
+image.SaveAsBmp(imageFileStream);
