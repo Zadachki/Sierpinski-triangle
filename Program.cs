f6e4890de742435f9294e1c1cc0a9ed52c7aebc0
aril114 <125197457+aril114@users.noreply.github.com>
2025-10-19 18:57:02 +0900

    Добавлен метод рисования трех треугольников

--- Program.cs
+++ Program.cs
@@ -14,21 +14,36 @@ DrawingOptions drawingOptions = new DrawingOptions();
 // Creates a new image with empty pixel data. 
 using (Image<Rgba32> image = new(width, height))
 {
-    DrawTriangle(image, new PointF(200f, 200.0f), 50f, Color.PaleVioletRed, false);
-    DrawTriangle(image, new PointF(400f, 200.0f), 50f, Color.Tan, true);
+    DrawTriforse(image, new PointF(200f, 200f), 50f, Color.Tan);
 
     image.SaveAsPng("serpinski triangle.png");
 } // Dispose - releasing memory into a memory pool ready for the next image you wish to process.
 
-void DrawTriangle(Image<Rgba32> image, PointF blPoint, float sideLength, Color color, bool fill)
+void DrawTriforse(Image<Rgba32> image, PointF tcPoint, float sideLength, Color color)
 {
-    float height = float.Sqrt(3) * sideLength / 2;
+    var triangleHeight = float.Sqrt(3) * sideLength / 2f;
+
+    var topTriangleBlPoint = new PointF(tcPoint.X - sideLength / 2f, tcPoint.Y + triangleHeight);
+
+    DrawTriangle(image, topTriangleBlPoint, sideLength, color);
+
+    var leftTriangleBlPoint = new PointF(topTriangleBlPoint.X - sideLength / 2f, topTriangleBlPoint.Y + triangleHeight);
+
+    DrawTriangle(image, leftTriangleBlPoint, sideLength, color);
+
+    var rightTriangleBlPoint = new PointF(tcPoint.X, leftTriangleBlPoint.Y);
+
+    DrawTriangle(image, rightTriangleBlPoint, sideLength, color);
+}
+
+void DrawTriangle(Image<Rgba32> image, PointF blPoint, float sideLength, Color color)
+{
+    float height = float.Sqrt(3f) * sideLength / 2f;
 
     PointF[] points = new PointF[] {
         blPoint,
         new PointF(blPoint.X + sideLength, blPoint.Y),
-        new PointF(blPoint.X + sideLength / 2.0f, blPoint.Y - height),
-        blPoint
+        new PointF(blPoint.X + sideLength / 2.0f, blPoint.Y - height)
     };
 
     Path path = new Path(points);
@@ -36,8 +51,5 @@ void DrawTriangle(Image<Rgba32> image, PointF blPoint, float sideLength, Color c
     SolidPen pen = Pens.Solid(color, 1);
 
     image.Mutate(img => img
-        .Draw(drawingOptions, pen, path));
-
-    if (fill)
-        image.Mutate(img => img.Fill(color, path));
+        .Fill(color, path));
 }
